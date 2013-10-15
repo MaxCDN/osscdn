81b0358473d35e000219750f971da2497dd03de8
Juho Vepsalainen <bebraw@gmail.com>
2013-10-15 13:20:24 +0300

    Improve frontend structure and update Angular
    Note that ng-if seems glitchy in RC3 so I ended up replacing those with
    ng-show. As a result animations were disabled.
    
    While at it I removed a couple of redundant dependencies from the
    project and added routing dependency. That is useful particularly if we
    want to do something more special like linking to a specific library.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,24 +1,10 @@
 'use strict';
 
-var app = angular.module('osscdnApp', ['ngAnimate']);
-
-app.controller('InstantSearchController', ['$scope', '$http', function($scope, $http) {
-    $http.get('data/index.json').then(function(res) {
-        $scope.libraries = res.data;
+angular.module('osscdnApp', ['ngAnimate', 'ngRoute']).config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    }).otherwise({
+        redirectTo: '/'
     });
-
-    $scope.getLibrary = function($index, library) {
-        library.showExtra =! library.showExtra;
-
-        if(library.description) return;
-
-        $http.get('data/' + library.name + '.json').then(function(res) {
-            var d = res.data;
-            var k;
-
-            for(k in d) {
-                library[k] = d[k];
-            }
-        });
-    }
-}]);
+});
--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
-angular.module('osscdnApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('osscdnApp').controller('MainCtrl', function ($scope, $http) {
+    $http.get('data/index.json').then(function(res) {
+        $scope.libraries = res.data;
+    });
+
+    $scope.getLibrary = function($index, library) {
+        library.showExtra =! library.showExtra;
+
+        if(library.description || !library.showExtra) return;
+
+        $http.get('data/' + library.name + '.json').then(function(res) {
+            var d = res.data;
+            var k;
+
+            for(k in d) {
+                library[k] = d[k];
+            }
+        });
+    }
+});
