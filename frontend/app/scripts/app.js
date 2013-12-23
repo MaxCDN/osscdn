a5082d32f82194c85ebaa17e5d77e848cc6b2954
Juho Vepsalainen <bebraw@gmail.com>
2013-12-23 17:08:11 +0200

    Enable html5mode for Angular
    Ok since we're not supporting old IE.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('osscdnApp', ['ngAnimate', 'ngRoute']).config(function ($routeProvider) {
+angular.module('osscdnApp', ['ngAnimate', 'ngRoute']).
+        config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
     $routeProvider.when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
