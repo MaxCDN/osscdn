d13d4c4233f691bbc74520cff3312cdb1f4640d8
Juho Vepsalainen <bebraw@gmail.com>
2013-12-24 21:12:48 +0200

    Add exclude example

--- walker/template.config.js
+++ walker/template.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    github: 'replacethis' // generate through GitHub settings -> Applications
+    github: 'replacethis', // generate through GitHub settings -> Applications
+    exclude: ['foo', 'bar'] // libraries to exclude (by project name)
 };
