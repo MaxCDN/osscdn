8fe33aebaf9d16f95f6f91453de16627bb81c1e6
Juho Vepsalainen <bebraw@gmail.com>
2013-12-28 19:39:09 +0200

    Watch CSS style changed at LiveReload

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
                 },
                 files: [
                     '<%= yeoman.app %>/{,*/}*.html',
-                    '.tmp/styles/{,*/}*.css',
+                    'app/styles/{,*/}*.css',
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
                 ]
