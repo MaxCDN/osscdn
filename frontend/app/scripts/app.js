5cd610673caf18a4a5c23c81edca4d70256e8f95
Juho Vepsalainen <bebraw@gmail.com>
2013-12-28 10:38:26 +0200

    Remove redundant view references

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -4,15 +4,10 @@ angular.module('osscdnApp', ['ngAnimate', 'ui.router', 'ngDropdowns']).
         config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('network', {
-        url: '/network',
-        templateUrl: 'views/network.html'
-    }).state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html'
-    }).state('libraries', {
+    $stateProvider.state('libraries', {
         url: '/:name',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
     });
 });
+
